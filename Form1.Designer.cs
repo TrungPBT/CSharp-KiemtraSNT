e8c34cf623f4f7d92e508a2a05839fe83fed65f2
SPC <SPC@23-LAB402>
2024-09-09 19:37:49 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CSharp_KiemtraSNT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_n = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_kq = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_sn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(425, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txt_n
+            // 
+            this.txt_n.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_n.Location = new System.Drawing.Point(167, 23);
+            this.txt_n.Name = "txt_n";
+            this.txt_n.Size = new System.Drawing.Size(146, 35);
+            this.txt_n.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nhập số ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kiểm Tra SNT";
+            // 
+            // txt_kq
+            // 
+            this.txt_kq.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_kq.Location = new System.Drawing.Point(216, 101);
+            this.txt_kq.Name = "txt_kq";
+            this.txt_kq.Size = new System.Drawing.Size(146, 35);
+            this.txt_kq.TabIndex = 3;
+            this.txt_kq.TextChanged += new System.EventHandler(this.txt_kq_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tìm SNT nhỏ hơn";
+            // 
+            // txt_sn
+            // 
+            this.txt_sn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sn.Location = new System.Drawing.Point(251, 178);
+            this.txt_sn.Name = "txt_sn";
+            this.txt_sn.Size = new System.Drawing.Size(146, 35);
+            this.txt_sn.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 316);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_sn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_kq);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_n);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tìm SNT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_n;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_kq;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_sn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_KiemtraSNT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool ktraSNT(int n)
+        {
+            int dem = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    dem++;
+                }
+            }
+            if (dem == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void txt_kq_TextChanged(object sender, EventArgs e)
+        {
+            string kq = string.Empty;
+            if (txt_n.Text != string.Empty)
+            {
+                if (ktraSNT(int.Parse(txt_n.Text)))
+                {
+                    txt_kq.Text = "Là SNT";
+                }
+                else
+                {
+                    txt_kq.Text = "Không Là SNT";
+                }
+                for (int i = 2; i < int.Parse(txt_n.Text); i++)
+                {
+                    if (ktraSNT(i))
+                    {
+                        kq += " " + i;
+                    }
+                }
+                txt_sn.Text = kq;
+            }
+        }
+    }
+}
